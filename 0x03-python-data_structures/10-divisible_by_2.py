0eb7dda29a78b7ff36f3b1a1e98e23c8a32e3302
simon jerry <swagasoft@gmail.com>
2023-03-12 13:46:30 -0700

    check for multiples

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+
+    multiples = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            multiples.append(True)
+        else:
+            multiples.append(False)
+
+    return (multiples)
