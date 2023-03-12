3e814955c0acebfac4889f349539547a9d33cfb0
simon jerry <swagasoft@gmail.com>
2023-03-12 12:14:58 -0700

    change len to instance of

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) > 0:
+    if isinstance(my_list, list):
         my_list.reverse()
         for i in my_list:
             print("{:d}".format(i))
