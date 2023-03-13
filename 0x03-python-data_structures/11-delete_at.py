188c83702dd7995c125acc8b469b8a691791e07d
simon jerry <swagasoft@gmail.com>
2023-03-13 15:14:46 -0700

    delete index

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=[], idx=0):
+    """Delete an item at a specific position in a list."""
+    if idx >= 0 and idx < len(my_list):
+        del my_list[idx]
+    return (my_list)
