2aa13fdb69293322030872d73edb2bf3bed4bfa3
simon jerry <swagasoft@gmail.com>
2023-03-15 15:19:47 -0700

    print matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[num ** 2 for num in row] for row in matrix]
-    return
+    new_matrix = [row[:] for row in matrix]
+    for idx, row in enumerate(new_matrix):
+        for idx2, col in enumerate(new_matrix):
+            new_matrix[idx][idx2] = row[idx2] ** 2
+    return new_matrix 
