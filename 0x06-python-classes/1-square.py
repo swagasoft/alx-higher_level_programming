691bda970a06c2969a89d87c5130a65798e1cb23
simon jerry <swagasoft@gmail.com>
2023-03-28 13:08:06 -0700

    correct string val

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-"""Square class defination"""
+"""Define a class Square."""
 
 
 class Square:
+    """Represent a square."""
 
     def __init__(self, size):
-        """class contructor
-        Args: 
-        size (int): The size of the new square.
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
         """
         self.__size = size
