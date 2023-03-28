377203493623e60a603012bf54804ddcc54614d6
simon jerry <swagasoft@gmail.com>
2023-03-28 13:02:59 -0700

    square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Square class defination"""
+
+
+class Square:
+
+    def __init__(self, size):
+        """class contructor
+        Args: size (int): The size of the new square.
+        """
+        self.__size = size
