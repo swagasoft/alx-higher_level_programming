d9ab530af27646619e8cb267cb949a779bab3510
simon jerry <swagasoft@gmail.com>
2023-04-12 12:27:40 -0700

    override equal and not equal

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class MyInt that inherits from int."""
+
+
+class MyInt(int):
+    """MyInt class body"""
+
+    def __eq__(self, value):
+        """Override == opeartor with !=."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with ==."""
+        return self.real == value 
