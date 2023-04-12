93a242d4d4f7cd4539455f385a38b06427d78f60
simon jerry <swagasoft@gmail.com>
2023-04-12 12:17:15 -0700

    task 11-square done

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines Rectangle module."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class body."""
+
+    def __init__(self, size):
+        
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
