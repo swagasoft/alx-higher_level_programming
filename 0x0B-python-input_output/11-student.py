4675352b23d389ebea14a8f49f7e7da9a70dd646
simon jerry <swagasoft@gmail.com>
2023-04-13 14:36:09 -0700

    student task

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student props
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            setattr(self, k, v)
