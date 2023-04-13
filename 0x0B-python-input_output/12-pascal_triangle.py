27c7de3149761f4d1de86b23fa7243a372370491
simon jerry <swagasoft@gmail.com>
2023-04-13 14:40:44 -0700

    pascal traigle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""pascal_traigle module."""
+
+
+def pascal_triangle(n):
+    """ pascal traigle class body.
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return 
