04f26d2dbd01d2e2c9e6f860b53d5f7a9c822dd4
simon jerry <swagasoft@gmail.com>
2023-05-17 14:13:53 -0700

    order by score

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- List all records with score >= 10 in 'second_table' of db 'hbtn_0c_0'
+-- Results should display both score and name
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
