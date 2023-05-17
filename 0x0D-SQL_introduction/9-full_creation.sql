932a89226603e1edd1c49216ab4bf2957b71575c
simon jerry <swagasoft@gmail.com>
2023-05-17 14:08:48 -0700

    full createtion

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- Create table 'second_table' in db 'hbtn_0c_0'
+-- Addprops  (id INT), (name VARCHAR(256)), (score INT)
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+
+-- Script should create 4 records
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
