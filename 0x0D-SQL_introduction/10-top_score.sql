1991a53f54083db985b291f8ed3c5c6f08d6242b
simon jerry <swagasoft@gmail.com>
2023-05-17 14:11:40 -0700

    order item

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- List all records in 'second_table' of db 'hbtn_0c_0'
+-- Results should display both score and name
+-- Records should be ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
