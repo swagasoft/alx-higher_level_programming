296939d29c2dbb4322b8dc157fd28dc2dbabbc87
simon jerry <swagasoft@gmail.com>
2023-06-20 07:18:41 -0700

    corrections

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Write a python file that contains the class definition of a State and an instance Base.
+Write a python file that contains the class definition of a State 
+and an instance Base.
 """
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
