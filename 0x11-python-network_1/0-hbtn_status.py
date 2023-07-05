7ce32e6ac28867b7fc0a69690f694fc24491ff8b
simon jerry <swagasoft@gmail.com>
2023-07-05 08:47:59 -0700

    task one

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Fetches https://intranet.hbtn.io/status."""
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(html.__class__))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
