4f8be45abd18ef9a51b909aff0c21856cd8fd1e3
simon jerry <swagasoft@gmail.com>
2023-07-05 14:29:47 -0700

    tax found

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Uses the GitHub API to display a GitHub ID based on given credentials.
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
